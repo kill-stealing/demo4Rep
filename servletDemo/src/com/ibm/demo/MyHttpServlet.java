1bc0f1476197878f80fbdb439b5a844a3dd96883
sven <13840935238@163.com>
2017-05-09 10:03:36 +0800

    add

---
+++ servletDemo/src/com/ibm/demo/MyHttpServlet.java
@@ -0,0 +1,32 @@
+package com.ibm.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyHttpServlet
+ */
+public class MyHttpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		String encodingString=this.getServletConfig().getInitParameter("encoding");
+		response.getWriter().println(encodingString);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
