060fca0efd2946e36c99b191163d97ab599a73a5
sven <dllmyliu@cn.ibm.com>
2017-09-01 22:49:12 +0800

    add code 0901

--- servletDemo/WebContent/js中级/第01课：DOM基础概念、操作/js/main.js
+++ servletDemo/WebContent/js中级/第01课：DOM基础概念、操作/js/main.js
@@ -1,6 +1,4 @@
-function getStyle(obj,attr){
-	return obj.currentStyle?obj.currentStyle[attr]:getComputedStyle(obj,0)[attr];
-}
+
 function $(id){return document.getElementById(id);}
 
 function doMove(obj,attr,step,target,endFn){
@@ -60,4 +58,39 @@ function doChangeOpacity(obj,perTime,speed,target,endFn){
 			endFn&&endFn();
 		}
 	},perTime); 
+}
+
+function getStyle(obj,attr){
+	return obj.currentStyle?obj.currentStyle[attr]:getComputedStyle(obj,0)[attr];
+}
+
+function startMove(obj,attr,iTarget,fn){
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		//取当前值
+		var iCur=0;
+		if(attr=='opacity'){
+			iCur=parseInt(parseFloat(getStyle(obj,attr))*100);
+		}else{
+			iCur=parseInt(getStyle(obj,attr));
+		}
+		
+		var iSpeed=(iTarget-iCur)/8;
+		iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
+		
+		if(iCur==iTarget){
+			clearInterval(obj.timer);
+			if(fn){
+				fn();
+			}
+		}else{
+			if(attr=='opacity'){
+				obj.style.filter='alpha(opacity:'+(iCur+iSpeed)+')';
+				obj.style.opacity=(iCur+iSpeed)/100;
+			}
+			else{
+				obj.style[attr]=iCur+iSpeed+"px";
+			}
+		}
+	},30);
 }
