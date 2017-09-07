52b6d8515c7e4fd3380d5bc5ddcdc06d73f47201
sven <dllmyliu@cn.ibm.com>
2017-09-07 22:16:57 +0800

    add code 0907

---
+++ servletDemo/WebContent/js高级/其他专题/move.js
@@ -0,0 +1,69 @@
+
+function getStyle(obj,attr){
+	return obj.currentStyle?obj.currentStyle[attr]:getComputedStyle(obj,0)[attr];
+}
+
+function startMove(obj,attr,iTarget,fn){
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		//取当前值
+		var iCur=0;
+		if(attr=='opacity'){
+			iCur=parseInt(parseFloat(getStyle(obj,attr))*100);
+		}else{
+			iCur=parseInt(getStyle(obj,attr));
+		}
+		
+		var iSpeed=(iTarget-iCur)/8;
+		iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
+		if(iCur==iTarget){
+			clearInterval(obj.timer);
+			if(fn){
+				fn();
+			}
+		}else{
+			if(attr=='opacity'){
+				obj.style.filter='alpha(opacity:'+(iCur+iSpeed)+')';
+				obj.style.opacity=(iCur+iSpeed)/100;
+			}
+			else{
+				obj.style[attr]=iCur+iSpeed+"px";
+			}
+		}
+	},30);
+}
+
+function startMoveNew(obj,json,fn){
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		var ifStop=true;
+		for(var attr in json){
+			//取当前值
+			var iCur=0;
+			if(attr=='opacity'){
+				iCur=parseInt(parseFloat(getStyle(obj,attr))*100);
+			}else{
+				iCur=parseInt(getStyle(obj,attr));
+			}
+			
+			var iSpeed=(json[attr]-iCur)/8;
+			iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
+			if(iCur!=json[attr]){
+				ifStop=false;
+			}
+			if(attr=='opacity'){
+				obj.style.filter='alpha(opacity:'+(iCur+iSpeed)+')';
+				obj.style.opacity=(iCur+iSpeed)/100;
+			}
+			else{
+				obj.style[attr]=iCur+iSpeed+"px";
+			}
+		}
+		if(ifStop){
+			clearInterval(obj.timer);
+			if(fn){
+				fn();
+			}
+		}
+	},30);
+}
